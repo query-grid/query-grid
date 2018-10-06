d2ee47a609ff75d943bc9822fef657145ee2cca7
Andrew Willis <andrew@willishq.co.uk>
2018-10-07 00:05:05 +0100

    removed old filter implementation

--- src/Column.php
+++ src/Column.php
@@ -30,16 +30,11 @@ class Column
      * @var Callable|null
      */
     private $formatter;
-    /**
-     * @var string
-     */
-    private $filter;
 
     public function __construct(string $key, string $label)
     {
         $this->key = $key;
         $this->label = $label;
-        $this->filter = [Filter::class, 'create'];
     }
 
     public function getKey(): string
