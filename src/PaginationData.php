ca40a1aada8bbfb28c51656d8f212d77d100a473
Andrew Willis <andrew@willishq.co.uk>
2018-10-10 21:08:38 +0100

    phpcs autofix test method, pagination builder

--- src/PaginationData.php
+++ src/PaginationData.php
@@ -2,12 +2,16 @@
 
 namespace Willishq\QueryGrid;
 
-class PaginationData
+use Willishq\QueryGrid\Contracts\Arrayable;
+
+class PaginationData implements Arrayable
 {
     /** @var int */
     private $perPage;
     /** @var int */
     private $itemCount = 0;
+    private $totalItems;
+    private $currentPage = 1;
 
     public function __construct(int $perPage)
     {
@@ -21,23 +25,55 @@ class PaginationData
 
     public function getItemCount(): int
     {
-        return $this->itemCount;
+        if (is_null($this->totalItems)) {
+            return $this->itemCount;
+        }
+        return $this->getLastPage() === $this->getCurrentPage()
+            ? $this->getTotalItems() % $this->getPerPage()
+            : $this->getPerPage();
     }
+
     public function getTotalItems(): ?int
     {
-        return null;
+        return $this->totalItems;
     }
+
     public function getCurrentPage(): int
     {
-        return 1;
+        return $this->currentPage;
     }
+
     public function getLastPage(): ?int
     {
-        return null;
+        if (is_null($this->totalItems)) {
+            return null;
+        }
+        return intval(ceil($this->totalItems / $this->perPage), 10);
     }
 
     public function setItemCount(int $itemCount)
     {
         $this->itemCount = $itemCount;
+    }
+
+    public function setTotalItems(int $totalItems)
+    {
+        $this->totalItems = $totalItems;
+    }
+
+    public function setCurrentPage(int $currentPage)
+    {
+        $this->currentPage = $currentPage;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'perPage' => $this->getPerPage(),
+            'itemCount' =>$this->getItemCount(),
+            'currentPage' =>$this->getCurrentPage(),
+            'totalItems' => $this->getTotalItems(),
+            'lastPage' =>$this->getLastPage(),
+        ];
     }
 }
--- tests/PaginationDataTest.php
+++ tests/PaginationDataTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Willishq\QueryGrid\Contracts\Arrayable;
 use Willishq\QueryGrid\PaginationData;
 
 class PaginationDataTest extends TestCase
@@ -16,7 +17,9 @@ class PaginationDataTest extends TestCase
         $this->assertEquals(1, $paginationData->getCurrentPage());
         $this->assertEquals(null, $paginationData->getTotalItems());
         $this->assertEquals(null, $paginationData->getLastPage());
-    }    /** @test */
+    }
+
+    /** @test */
     public function itSetsItemCount()
     {
         $paginationData = new PaginationData(25);
@@ -27,5 +30,51 @@ class PaginationDataTest extends TestCase
         $this->assertEquals(1, $paginationData->getCurrentPage());
         $this->assertEquals(null, $paginationData->getTotalItems());
         $this->assertEquals(null, $paginationData->getLastPage());
+    }
+
+
+    /** @test */
+    public function itSetsTotalItems()
+    {
+        $paginationData = new PaginationData(10);
+        $paginationData->setTotalItems(100);
+
+        $this->assertEquals(10, $paginationData->getPerPage());
+        $this->assertEquals(10, $paginationData->getItemCount());
+        $this->assertEquals(1, $paginationData->getCurrentPage());
+        $this->assertEquals(100, $paginationData->getTotalItems());
+        $this->assertEquals(10, $paginationData->getLastPage());
+    }
+
+
+    /** @test */
+    public function itGetsCorrectItemCountForCurrentPage()
+    {
+        $paginationData = new PaginationData(10);
+        $paginationData->setTotalItems(64);
+        $paginationData->setCurrentPage(7);
+
+        $this->assertEquals(10, $paginationData->getPerPage());
+        $this->assertEquals(4, $paginationData->getItemCount());
+        $this->assertEquals(7, $paginationData->getCurrentPage());
+        $this->assertEquals(64, $paginationData->getTotalItems());
+        $this->assertEquals(7, $paginationData->getLastPage());
+    }
+
+    /** @test */
+    public function itIsArrayable()
+    {
+        $paginationData = new PaginationData(10);
+        $paginationData->setTotalItems(64);
+        $paginationData->setCurrentPage(7);
+
+        $this->assertInstanceOf(Arrayable::class, $paginationData);
+        $this->assertEquals([
+            'perPage' => 10,
+            'itemCount' => 4,
+            'currentPage' => 7,
+            'totalItems' => 64,
+            'lastPage' => 7,
+        ], $paginationData->toArray());
     }
 }
